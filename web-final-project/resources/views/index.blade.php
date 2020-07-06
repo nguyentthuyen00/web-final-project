48fd32ce603b1d6ad6ba15f4444e7d51a3679f58
Thanh Tuấn <thanhtuanhk13@gmail.com>
2020-07-06 15:32:22 +0700

    update_3:32_7/6/2020

---
+++ web-final-project/resources/views/index.blade.php
@@ -0,0 +1,250 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+
+                <!-- Dai Cuong -->
+                <div class="blog-list clearfix">
+                    <div class="section-title">
+                        <h3 class="color-blue"><a href="#" title="">Đại Cương</a></h3>
+                    </div>
+                    <!-- end title -->
+
+                    <!-- Giai tich -->
+                    <div class="blog-box row">
+                        <div class="col-md-4">
+                            <div class="post-media">
+                                <a href="page-subject.html" title="">
+                                    <img src="upload/blog_square_01.jpg" alt="" class="img-fluid">
+                                    <div class="hovereffect"></div>
+                                </a>
+                            </div>
+                            <!-- end media -->
+                        </div>
+                        <!-- end col -->
+
+                        <div class="blog-meta big-meta col-md-8">
+                            <h4><a href="page-subject.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                            <small><a href="page-subject.html" title="">Giải tích</a></small>
+                            <small><a href="#" title="">01/05/2020</a></small>
+                            <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                        </div>
+                        <!-- end meta -->
+                    </div>
+                    <!-- end blog-box -->
+
+                    <hr class="invis">
+
+                    <!-- Tu tuong -->
+                    <div class="blog-box row">
+                        <div class="col-md-4">
+                            <div class="post-media">
+                                <a href="page-subject.html" title="">
+                                    <img src="upload/blog_square_01.jpg" alt="" class="img-fluid">
+                                    <div class="hovereffect"></div>
+                                </a>
+                            </div>
+                            <!-- end media -->
+                        </div>
+                        <!-- end col -->
+
+                        <div class="blog-meta big-meta col-md-8">
+                            <h4><a href="page-subject.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                            <small><a href="page-subject.html" title="">Tư tưởng Hồ Chí Minh</a></small>
+                            <small><a href="#" title="">01/05/2020</a></small>
+                            <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                        </div>
+                        <!-- end meta -->
+                    </div>
+                    <!-- end blog-box -->
+
+                    <hr class="invis">
+
+                    <!-- Phap luat dai cuong -->
+                    <div class="blog-box row">
+                        <div class="col-md-4">
+                            <div class="post-media">
+                                <a href="page-subject.html" title="">
+                                    <img src="upload/blog_square_01.jpg" alt="" class="img-fluid">
+                                    <div class="hovereffect"></div>
+                                </a>
+                            </div>
+                            <!-- end media -->
+                        </div>
+                        <!-- end col -->
+
+                        <div class="blog-meta big-meta col-md-8">
+                            <h4><a href="page-subject.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                            <small><a href="page-subject.html" title="">Pháp luật đại cương</a></small>
+                            <small><a href="#" title="">01/05/2020</a></small>
+                            <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                        </div>
+                        <!-- end meta -->
+                    </div>
+                    <!-- end blog-box -->
+                </div>
+                <!-- end blog-list -->
+
+                <hr class="invis">
+
+                <!-- Chuyen Nganh -->
+                <div class="blog-list clearfix">
+                    <div class="section-title">
+                        <h3 class="color-purple"><a href="#" title="">Chuyên Ngành</a></h3>
+                    </div>
+                    <!-- end title -->
+
+                    <!-- Nhap mon lap trinh -->
+                    <div class="blog-box row">
+                        <div class="col-md-4">
+                            <div class="post-media">
+                                <a href="page-subject.html" title="">
+                                    <img src="upload/blog_square_01.jpg" alt="" class="img-fluid">
+                                    <div class="hovereffect"></div>
+                                </a>
+                            </div>
+                            <!-- end media -->
+                        </div>
+                        <!-- end col -->
+
+                        <div class="blog-meta big-meta col-md-8">
+                            <h4><a href="page-subject.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                            <small><a href="page-subject.html" title="">Nhập môn lập trình</a></small>
+                            <small><a href="#" title="">01/05/2020</a></small>
+                            <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                        </div>
+                        <!-- end meta -->
+                    </div>
+                    <!-- end blog-box -->
+
+                    <hr class="invis">
+
+                    <!-- Tiep thi can ban -->
+                    <div class="blog-box row">
+                        <div class="col-md-4">
+                            <div class="post-media">
+                                <a href="page-subject.html" title="">
+                                    <img src="upload/blog_square_01.jpg" alt="" class="img-fluid">
+                                    <div class="hovereffect"></div>
+                                </a>
+                            </div>
+                            <!-- end media -->
+                        </div>
+                        <!-- end col -->
+
+                        <div class="blog-meta big-meta col-md-8">
+                            <h4><a href="page-subject.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                            <small><a href="page-subject.html" title="">Tiếp thị căn bản</a></small>
+                            <small><a href="#" title="">01/05/2020</a></small>
+                            <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                        </div>
+                        <!-- end meta -->
+                    </div>
+                    <!-- end blog-box -->
+
+                    <hr class="invis">
+
+                    <!-- Lap trinh huong doi tuong -->
+                    <div class="blog-box row">
+                        <div class="col-md-4">
+                            <div class="post-media">
+                                <a href="page-subject.html" title="">
+                                    <img src="upload/blog_square_01.jpg" alt="" class="img-fluid">
+                                    <div class="hovereffect"></div>
+                                </a>
+                            </div>
+                            <!-- end media -->
+                        </div>
+                        <!-- end col -->
+
+                        <div class="blog-meta big-meta col-md-8">
+                            <h4><a href="page-subject.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                            <small><a href="page-subject.html" title="">Lập trình hướng đối tượng</a></small>
+                            <small><a href="#" title="">01/05/2020</a></small>
+                            <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                        </div>
+                        <!-- end meta -->
+                    </div>
+                    <!-- end blog-box -->
+                </div>
+                <!-- end blog-list -->
+            </div>
+            <!-- end col -->
+
+            <div class="col-lg-3">
+                <div class="section-title">
+                    <h3 class="color-red"><a href="#" title="">Videos Học Tập</a></h3>
+                </div>
+                <!-- end title -->
+
+                <!-- Video hoc Nhap mon lap trinh -->
+                <div class="blog-box">
+                    <div class="post-media">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLjzaUXKQiFUQLytVOdvOy9GEA3GeLjWWk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!-- end media -->
+                    <div class="blog-meta">
+                        <h4><a href="single.html" title="">Lý thuyết lưu đồ thuật toán</a></h4>
+                        <small><a href="page-video.html" title="">Nhập môn lập trình</a></small>
+                        <small><a href="#" title="">01/05/2020</a></small>
+                    </div>
+                    <!-- end meta -->
+                </div>
+                <!-- end blog-box -->
+
+                <hr class="invis">
+
+                <!-- Video hoc OOP -->
+                <div class="blog-box">
+                    <div class="post-media">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/FKpkpHN3zO8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!-- end media -->
+                    <div class="blog-meta">
+                        <h4><a href="#" title="">Giải hệ phương trình tuyến tính bằng phương pháp ma trận nghịch đảo</a></h4>
+                        <small><a href="page-video.html" title="">Đại số tuyến tính</a></small>
+                        <small><a href="#" title="">01/05/2020</a></small>
+                    </div>
+                    <!-- end meta -->
+                </div>
+                <!-- end blog-box -->
+
+                <hr class="invis">
+
+                <!-- Video hoc Xac suat thong ke -->
+                <div class="blog-box">
+                    <div class="post-media">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/Nn54QK0wdeE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!-- end media -->
+
+                    <div class="blog-meta">
+                        <h4><a href="#" title="">Tính xác suất bằng định nghĩa</a></h4>
+                        <small><a href="page-video.html" title="">Xác suất thống kê</a></small>
+                        <small><a href="#" title="">01/05/2020</a></small>
+                    </div>
+                    <!-- end meta -->
+                </div>
+                <!-- end blog-box -->
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+</section>
+@endsection
---
+++ web-final-project/resources/views/page-video.blade.php
@@ -0,0 +1,157 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="page-title wb">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                <h2><i class="fa fa-play bg-blue"></i> Videos học tập <small class="hidden-xs-down hidden-sm-down">Ở đây có Videos học tập</small></h2>
+            </div>
+            <!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 hidden-xs-down hidden-sm-down">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Trang chủ</a></li>
+                    <li class="breadcrumb-item active">Videos học tập</li>
+                </ol>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</div>
+<!-- end page-title -->
+
+<section class="section wb">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="page-wrapper">
+                    <div class="blog-video-system">
+                        <div class="row">
+
+                            <div class="col-md-12">
+                                <div class="blog-box">
+                                    <div class="post-media">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLjzaUXKQiFUQLytVOdvOy9GEA3GeLjWWk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                    <!-- end media -->
+                                    <div class="blog-meta big-meta">
+                                        <h4><a href="single.html" title="">Nhập Môn Lập Trình</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh.</p>
+                                        <small><a href="blog-category-01.html" title="">Videos học tập</a></small>
+                                        <small><a href="single.html" title="">01/05/2020</a></small>
+                                        <small><a href="blog-author.html" title="">Ecautopass Team</a></small>
+                                    </div>
+                                    <!-- end meta -->
+                                    <a href="single-video.html" class="btn btn-primary">Xem Thêm</a>
+                                </div>
+                                <!-- end blog-box -->
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-md-12">
+                                <div class="blog-box">
+                                    <div class="post-media">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLjzaUXKQiFUTUjmu0Z8Sp2-gf0hzIxXbZ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                    <!-- end media -->
+                                    <div class="blog-meta big-meta">
+                                        <h4><a href="single.html" title="">Lập Trình Hướng Đối Tượng</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh.</p>
+                                        <small><a href="blog-category-01.html" title="">Videos học tập</a></small>
+                                        <small><a href="single.html" title="">01/05/2020</a></small>
+                                        <small><a href="blog-author.html" title="">Ecautopass Team</a></small>
+                                    </div>
+                                    <!-- end meta -->
+                                    <a href="single-video.html" class="btn btn-primary">Xem Thêm</a>
+                                </div>
+                                <!-- end blog-box -->
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-md-12">
+                                <div class="blog-box">
+                                    <div class="post-media">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL2Atg2BRb6GuXCcnTK5NeYXunbW_LbZcD" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                    <!-- end media -->
+                                    <div class="blog-meta big-meta">
+                                        <h4><a href="single.html" title="">Đại Số Tuyến Tính</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh.</p>
+                                        <small><a href="blog-category-01.html" title="">Videos học tập</a></small>
+                                        <small><a href="single.html" title="">01/05/2020</a></small>
+                                        <small><a href="blog-author.html" title="">Ecautopass Team</a></small>
+                                    </div>
+                                    <!-- end meta -->
+                                    <a href="single-video.html" class="btn btn-primary">Xem Thêm</a>
+                                </div>
+                                <!-- end blog-box -->
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-md-12">
+                                <div class="blog-box">
+                                    <div class="post-media">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL2Atg2BRb6Gvv5VInHzSJqGqyM4xgziUJ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                    <!-- end media -->
+                                    <div class="blog-meta big-meta">
+                                        <h4><a href="single.html" title="">Xác Suất Thống Kê </a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh.</p>
+                                        <small><a href="blog-category-01.html" title="">Videos học tập</a></small>
+                                        <small><a href="single.html" title="">01/05/2020</a></small>
+                                        <small><a href="blog-author.html" title="">Ecautopass Team</a></small>
+                                    </div>
+                                    <!-- end meta -->
+                                    <a href="single-video.html" class="btn btn-primary">Xem Thêm</a>
+                                </div>
+                                <!-- end blog-box -->
+                            </div>
+                            <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+                        <hr class="invis3">
+
+                    </div>
+                    <!-- end blog-grid-system -->
+                </div>
+                <!-- end page-wrapper -->
+
+                <hr class="invis3">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav aria-label="Page navigation">
+                            <ul class="pagination justify-content-start">
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="page-404.html">2</a></li>
+                                <li class="page-item"><a class="page-link" href="page-404.html">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</section>
+
+@endsection
