17507ce34940da5d4df8461150b7635c6d6ee28b
Thanh Tuấn <thanhtuanhk13@gmail.com>
2020-07-09 15:22:16 +0700

    Revert "upload database"
    This reverts commit 09cdeca2945b2bb496ec7760128f9f0f4405f970.

--- page-subject.php
+++ page-subject.php
@@ -1,4 +1,4 @@
-<!-- <?php 
+<?php 
     $conn = mysqli_connect("localhost","root","","web-final-project");
     mysqli_set_charset($conn,"utf8");
     $monhoc = "js";
@@ -11,113 +11,283 @@
         $result = $conn->query($sql)->fetch_assoc();
     
     
-        ?> -->
-        <?php include 'layout/header.php' ?>
+?>
+<?php include 'layout/header.php' ?>
 
-        <div class="page-title wb">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                        <h2><i class="fa fa-user bg-blue"></i> Môn học <small class="hidden-xs-down hidden-sm-down"> Ở đây có tài liệu môn học </small></h2>
-                    </div>
-                    <!-- end col -->
-                    <div class="col-lg-4 col-md-4 col-sm-12 hidden-xs-down hidden-sm-down">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.php">Trang chủ</a></li>
-                            <li class="breadcrumb-item"><a href="page-subject.php">Môn học</a></li>
-                        </ol>
-                    </div>
-                    <!-- end col -->
-                </div>
-                <!-- end row -->
+<div class="page-title wb">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                <h2><i class="fa fa-user bg-blue"></i> Môn học <small class="hidden-xs-down hidden-sm-down"> Ở đây có tài liệu môn học </small></h2>
             </div>
-            <!-- end container -->
+            <!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 hidden-xs-down hidden-sm-down">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.php">Trang chủ</a></li>
+                    <li class="breadcrumb-item"><a href="page-subject.php">Môn học</a></li>
+                </ol>
+            </div>
+            <!-- end col -->
         </div>
-        <!-- end page-title -->
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</div>
+<!-- end page-title -->
 
-        <section class="section wb">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 offset-md-2">
-                        <div class="page-wrapper">
-                            <div class="portfolio row">
-                                <?php
-                                $connection = mysqli_connect("localhost", "root", "", "web-final-project");
-                                mysqli_set_charset($connection,"utf8");
-                                $monhoc = "js";
-                                if(isset($_GET['subject'])){
-                                    $monhoc = $_GET['subject'];
-                                }
+<section class="section wb">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="page-wrapper">
+                    <div class="portfolio row">
 
-                                $sql="SELECT s.IdSubject,e.NameExam , e.Link 
-                                FROM subject s INNER JOIN exam e ON s.IdSubject=e.IdSubject 
-                                WHERE s.IdSubject = '$monhoc';";
 
-                                $result = mysqli_query($connection, $sql);
-
-                                
-                                while ($row=mysqli_fetch_row($result)): ?> 
-                                    <div class="pitem item-w1 item-h1">
-                                        <div class="blog-box">
-                                            <div class="post-media">
-                                                <a href="<?php echo "$row[2]" ?>" title="">
-                                                    <img src="upload/menu_01.jpg" alt="" class="img-fluid">
-                                                    <div class="hovereffect">
-                                                        <span></span>
-                                                    </div>
-                                                    <!-- end hover -->
-                                                </a>
-                                            </div>
-                                            <!-- end media -->
-                                            <div class="blog-meta">
-                                                <span class="bg-purple"><a href="<?php echo "$row[2]" ?>" title=""><?php echo "$row[1]" ?></a>
-                                                </span>
-
-                                            </div>
-                                            <span class="ml-2 pointer" d-id=345 data-toggle="tooltip" title="Tải xuống">
-                                                <i class="fa fa-download"><a href="<?php echo "$row[2]" ?>"></a></i>
-                                            </span>
-                                            <!-- end meta -->
-
+                        <div class="pitem item-w1 item-h1">
+                            <div class="blog-box">
+                                <div class="post-media">
+                                    
+                                    <a href="<?php echo "$result[Link]" ?>" title="">
+                                        <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                        <div class="hovereffect">
+                                            <span></span>
                                         </div>
-                                        <!-- end blog-box -->
-                                    </div>
-                                <?php endwhile; ?>
-                                <!--  end col -->
-
-
-
+                                        <!-- end hover -->
+                                    </a>
+                                </div>
+                                <!-- end media -->
+                                <div class="blog-meta">
+                                    <span class="bg-purple"><a href="<?php echo "$result[Link]" ?>" title=""><?php echo "$result[NameExam]" ?></a>
+                                    </span>
+                                    
+                                </div>
+                                <!-- end meta -->
 
                             </div>
-                            <!-- end portfolio -->
+                            <!-- end blog-box -->
                         </div>
-                        <!-- end page-wrapper -->
+                        <!-- end col -->
 
-                        <hr class="invis">
+                      <!--   <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 2</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 3</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 4</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 5</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 6</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 7</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 8</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col
+                      
+                      <div class="pitem item-w1 item-h1">
+                          <div class="blog-box">
+                              <div class="post-media">
+                                  <a href="#" title="">
+                                      <img src="upload/menu_01.jpg" alt="" class="img-fluid">
+                                      <div class="hovereffect">
+                                          <span></span>
+                                      </div>
+                                      end hover
+                                  </a>
+                              </div>
+                              end media
+                              <div class="blog-meta">
+                                  <span class="bg-purple"><a href="#" title="">Chương 9</a></span>
+                                  <h4><a href="#" title="">What is your favorite leather color</a></h4>
+                                  <small><a href="page-about-us.html" title="">Ecautopass Team</a></small>
+                                  <small><a href="#" title="">01/05/2020</a></small>
+                              </div>
+                              end meta
+                          </div>
+                          end blog-box
+                      </div>
+                      end col -->
+                    </div>
+                    <!-- end portfolio -->
+                </div>
+                <!-- end page-wrapper -->
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <nav aria-label="Page navigation">
-                                    <ul class="pagination justify-content-start">
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="page-404.html">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="page-404.html">3</a></li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="#">Next</a>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <!-- end col -->
-                        </div>
-                        <!-- end row -->
+                <hr class="invis">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav aria-label="Page navigation">
+                            <ul class="pagination justify-content-start">
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="page-404.html">2</a></li>
+                                <li class="page-item"><a class="page-link" href="page-404.html">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                     <!-- end col -->
                 </div>
                 <!-- end row -->
             </div>
-            <!-- end container -->
-        </section>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</section>
 
-
-        <?php include 'layout/footer.php' ?>
+<?php include 'layout/footer.php' ?>
