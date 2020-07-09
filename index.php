88e9c73d20231dd1be30ef9de88841f24f2f6214
huongnguyen00 <huongnguyen00@users.noreply.github.com>
2020-07-09 16:09:00 +0700

    Fix index page

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 
 <?php include 'layout/header.php' ?>
 
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
-
-<!-- Bootstrap CSS File -->
-<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Libraries CSS Files -->
-<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-<link href="lib/animate/animate.min.css" rel="stylesheet">
-<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-
-<!-- Main Stylesheet File -->
-<link href="css/style.css" rel="stylesheet">
-
-
         <!-- Slideshow -->
-        <div id="carouselExampleIndicators" class="carousel slide col-md-8 offset-md-2 header" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide col-md-8 offset-md-2 header" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -46,8 +29,6 @@
             </a>
         </div>
 
-<section class="section">
-</section>
 <!--==========================
       About Us Section
     ============================-->
@@ -429,21 +410,3 @@
 
     </section>
 <?php include 'layout/footer.php' ?>
-
-<!-- JavaScript Libraries -->
-<script src="lib/jquery/jquery.min.js"></script>
-<script src="lib/jquery/jquery-migrate.min.js"></script>
-<script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="lib/easing/easing.min.js"></script>
-<script src="lib/mobile-nav/mobile-nav.js"></script>
-<script src="lib/wow/wow.min.js"></script>
-<script src="lib/waypoints/waypoints.min.js"></script>
-<script src="lib/counterup/counterup.min.js"></script>
-<script src="lib/owlcarousel/owl.carousel.min.js"></script>
-<script src="lib/isotope/isotope.pkgd.min.js"></script>
-<script src="lib/lightbox/js/lightbox.min.js"></script>
-<!-- Contact Form JavaScript File -->
-<script src="contactform/contactform.js"></script>
-
-<!-- Template Main Javascript File -->
-<script src="js/main.js"></script>
