a899d442a013c5a0926f4e3e2172dc21c0fcb97f
Thanh Tuấn <thanhtuanhk13@gmail.com>
2020-07-10 11:47:07 +0700

    update page-subject

--- page-subject.php
+++ page-subject.php
@@ -1,23 +1,10 @@
-<!-- <?php 
-    $conn = mysqli_connect("localhost","root","","web-final-project");
-    mysqli_set_charset($conn,"utf8");
-    $monhoc = "js";
-    if(isset($_GET['subject'])){
-        $monhoc = $_GET['subject'];
-    }
-        $sql = "SELECT s.IdSubject,e.NameExam , e.Link 
-                FROM subject s INNER JOIN exam e ON s.IdSubject=e.IdSubject 
-                WHERE s.IdSubject = '$monhoc';";
-        $result = $conn->query($sql)->fetch_assoc();
-    
-    
-        ?> -->
-        <?php include 'layout/header.php' ?>
+   <?php include 'layout/header.php' ?>
 
         <div class="page-title wb">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12"><?php
+                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                        <?php
                                 $connection = mysqli_connect("localhost", "root", "", "web-final-project");
                                 mysqli_set_charset($connection,"utf8");
                                 $monhoc = "js";
@@ -25,9 +12,15 @@
                                     $monhoc = $_GET['subject'];
                                 }
 
+                                $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+                                $limit = 2;
+                                $start = ($current_page - 1) * $limit;
+                                $limitn = $start + $limit;
+
                                 $sql="SELECT s.IdSubject,e.NameExam , e.Link, s.NameSubject 
                                 FROM subject s INNER JOIN exam e ON s.IdSubject=e.IdSubject 
-                                WHERE s.IdSubject = '$monhoc';";
+                                WHERE s.IdSubject = '$monhoc'
+                                LIMIT $start,$limitn;";
 
                                 $result = mysqli_query($connection, $sql);
                                 $result1 = mysqli_query($connection, $sql);
@@ -39,7 +32,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-12 hidden-xs-down hidden-sm-down">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="index.php">Trang chủ</a></li>
-                            <li class="breadcrumb-item"><a href="./page-subject?subject=<?php echo $row[0] ?>"><?php echo $row[3] ?></a></li>
+                            <li class="breadcrumb-item"><a href="./page-subject?subject=$monhoc"><?php echo $row[3] ?></a></li>
                         </ol>
                     </div>
                     <!-- end col -->
@@ -68,13 +61,9 @@
                                         </div>
                                         <!-- end blog-box -->
                                     </div>
-                                    <br>
-                                <?php endwhile; ?>
+                                <?php endwhile;
+                                mysqli_close($connection); ?>
                                 <!--  end col -->
-
-
-
-
                             </div>
                             <!-- end portfolio -->
                         </div>
@@ -82,22 +71,74 @@
 
                         <hr class="invis">
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <nav aria-label="Page navigation">
-                                    <ul class="pagination justify-content-start">
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                       <!--  <div class="row">
+                           <div class="col-md-12">
+                               <nav aria-label="Page navigation">
+                                   <ul class="pagination justify-content-start"> -->
+                        
+                        <?php
+                        $connection = mysqli_connect("localhost", "root", "", "web-final-project");
+                                mysqli_set_charset($connection,"utf8");
+                                $monhoc = "js";
+                                if(isset($_GET['subject'])){
+                                    $monhoc = $_GET['subject'];
+                                }
+                    
+                    $sql = "SELECT count(IdExam) AS total FROM exam WHERE IdSubject='$monhoc'; ";
+                    $resulttotal =  mysqli_query($connection, $sql);
+                    $row1 = mysqli_fetch_assoc($resulttotal);
+                    
+                    $totalrecords = $row1['total'];
+
+                                    //find limit and current page
+                    $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+                    $limit = 2;
+
+                                    //find total page and start
+                    $total_page = ceil($totalrecords / $limit);
+                    if ($current_page > $total_page){
+                        $current_page = $total_page;
+                    }
+                    else if ($current_page < 1){
+                        $current_page = 1;
+                    }
+
+                    $start = ($current_page - 1) * $limit;
+
+
+
+                                    //display page split
+                    if ($current_page > 1 && $total_page > 1){
+                        echo '<a href="./page-subject.php?subject='.$monhoc.'&page='.($current_page-1).'">Prev</a> | ';
+                    }
+                    for ($i = 1; $i <= $total_page; $i++){
+                                        // Nếu là trang hiện tại thì hiển thị thẻ span
+                                        // ngược lại hiển thị thẻ a
+                        if ($i == $current_page){
+                            echo '<span>'.$i.'</span> | ';
+                        }
+                        else{
+                            echo '<a href="./page-subject.php?subject='.$monhoc.'&page='.$i.'">'.$i.'</a> | ';
+                        }
+                    }
+                    if ($current_page < $total_page && $total_page > 1){
+                        echo '<a href="./page-subject.php?subject='.$monhoc.'&page='.($current_page+1).'">Next</a> | ';
+                    }
+                    
+                    ?>
+                        
+                                        <!-- <li class="page-item"><a class="page-link" href="#">1</a></li>
                                         <li class="page-item"><a class="page-link" href="page-404.html">2</a></li>
                                         <li class="page-item"><a class="page-link" href="page-404.html">3</a></li>
                                         <li class="page-item">
                                             <a class="page-link" href="#">Next</a>
                                         </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <!-- end col -->
-                        </div>
-                        <!-- end row -->
+                                                                            </ul>
+                                                                        </nav>
+                                                                    </div>
+                                                                    end col
+                                                                </div>
+                                                                end row -->
                     </div>
                     <!-- end col -->
                 </div>
